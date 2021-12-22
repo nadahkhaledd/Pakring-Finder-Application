b40a5c796ffcc37ed8e6d771efc6bf49f3e2689e
nadahkhaledd <nadahkhaledd@gmail.com>
2021-12-22 23:28:13 +0200

    added search bar

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:park_locator/widgets/searchBar.dart';
 import 'package:provider/provider.dart';
 
 
@@ -12,19 +13,31 @@ class search extends StatelessWidget{
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            height: MediaQuery.of(context).size.height*2/3,
-            width: MediaQuery.of(context).size.width,
-            child: GoogleMap(
-              initialCameraPosition: (currentLocation != null) ? (CameraPosition(target:
-              LatLng(currentLocation.latitude, currentLocation.longitude), zoom: 18.0))
-              : (CameraPosition(target: LatLng(30.0313, 31.2107), zoom: 9.0)),
+          Expanded(
+            child: Stack(
+              children: [
+                Container(
+                height: MediaQuery.of(context).size.height*2/3,
+                width: MediaQuery.of(context).size.width,
+                child: GoogleMap(
+                  initialCameraPosition: (currentLocation != null) ? (CameraPosition(target:
+                  LatLng(currentLocation.latitude, currentLocation.longitude), zoom: 20.0))
+                  : (CameraPosition(target: LatLng(30.0313, 31.2107), zoom: 9.0)),
 
-              zoomGesturesEnabled: true,
-              zoomControlsEnabled: true,
-              rotateGesturesEnabled: true,
-              myLocationButtonEnabled: true,
-              myLocationEnabled: true,
+                  zoomGesturesEnabled: true,
+                  zoomControlsEnabled: true,
+                  rotateGesturesEnabled: true,
+                  myLocationButtonEnabled: true,
+                  myLocationEnabled: true,
+                  padding: EdgeInsets.only(top: 270.0,),
+
+                ),
+              ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: searchBar(),
+                ),
+              ],
             ),
           )
         ],
---
+++ lib/widgets/searchBar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+class searchBar extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return FloatingSearchBar(
+      hint: 'enter location',
+      openAxisAlignment: 0.0,
+      axisAlignment: isPortrait ? 0.0 : -1.0 ,
+      elevation: 4.0,
+      automaticallyImplyDrawerHamburger: false,
+      transitionCurve: Curves.easeInOut,
+      transitionDuration: Duration(milliseconds: 500),
+      transition: CircularFloatingSearchBarTransition(),
+      debounceDelay: Duration(milliseconds: 500),
+
+      onQueryChanged: (query)
+      {
+        ///
+      },
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(icon: Icon(Icons.place),
+            onPressed: null,)
+        ),
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+
+        )
+      ],
+      builder: (context, transition){
+        return ClipRRect(
+          child: Material(
+            color: Colors.white,
+            child: Container(
+              height: 200.0,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text('Home'),
+                    subtitle: Text('other...'),
+
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+}
