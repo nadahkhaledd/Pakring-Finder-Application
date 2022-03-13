77778eeb2e6b1fd3a6e2f74250fe50537385da2c
nadahkhaledd <nadahkhaledd@gmail.com>
2022-03-13 22:02:25 +0200

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 import 'package:park_locator/screens/search.dart';
 import 'package:park_locator/services/geoLocator.dart';
 import 'package:provider/provider.dart';
@@ -17,12 +18,27 @@ class MyApp extends StatelessWidget {
     return FutureProvider(
       create: (context) => geoLocatorService.getCurrentLocation(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'parking locator',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: search(),
+        home: start(),
       ),
+    );
+  }
+}
+
+class start extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 6,
+      navigateAfterSeconds: new search(),
+      image: new Image.asset('assets/images/car.png'),
+      photoSize: 70.0,
+      loaderColor: Colors.red,
+      backgroundColor: Colors.white12,
     );
   }
 }
--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -6,43 +6,70 @@ import 'package:park_locator/widgets/searchBar.dart';
 import 'package:provider/provider.dart';
 
 
-class search extends StatelessWidget{
+class search extends StatefulWidget{
+  @override
+  State<search> createState() => _searchState();
+}
+
+class _searchState extends State<search> {
+
   @override
   Widget build(BuildContext context) {
     final currentLocation = Provider.of<Position>(context);
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Stack(
-              children: [
-                Container(
-                height: MediaQuery.of(context).size.height*2/3,
-                width: MediaQuery.of(context).size.width,
-                child: GoogleMap(
-                  initialCameraPosition: (currentLocation != null) ? (CameraPosition(target:
-                  LatLng(currentLocation.latitude, currentLocation.longitude), zoom: 20.0))
-                  : (CameraPosition(target: LatLng(30.0313, 31.2107), zoom: 9.0)),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: Stack(
+                children: [
+                  Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: GoogleMap(
+                    initialCameraPosition: (currentLocation != null) ? (CameraPosition(target:
+                    LatLng(currentLocation.latitude, currentLocation.longitude), zoom: 20.0))
+                    : (CameraPosition(target: LatLng(30.0313, 31.2107), zoom: 15.0)),
 
-                  zoomGesturesEnabled: true,
-                  zoomControlsEnabled: true,
-                  rotateGesturesEnabled: true,
-                  myLocationButtonEnabled: true,
-                  myLocationEnabled: true,
-                  padding: EdgeInsets.only(top: 270.0,),
+                    zoomGesturesEnabled: true,
+                    zoomControlsEnabled: true,
+                    rotateGesturesEnabled: true,
+                    myLocationButtonEnabled: true,
+                    myLocationEnabled: true,
+                    padding: EdgeInsets.only(top: 480.0,),
 
+                  ),
                 ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: searchBar(),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: FloatingActionButton.extended(
+                            onPressed: () {
+                              setState(() {
+                              });
+                            },
+                            isExtended: true,
+                            label: Text("    Find    ", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
+                            backgroundColor: Colors.red,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
               ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: searchBar(),
-                ),
-              ],
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
-
 }
