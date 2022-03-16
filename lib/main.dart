d3ed931f64bd28a7c50f89fd52bee9426d051390
nadahossamm <63651482+nadahossamm@users.noreply.github.com>
2022-03-16 19:21:19 +0200

    marked page

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:park_locator/screens/search.dart';
 import 'package:park_locator/services/geoLocator.dart';
 import 'package:provider/provider.dart';
 
+import 'marked/MarkedPlaces.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -17,11 +19,12 @@ class MyApp extends StatelessWidget {
     return FutureProvider(
       create: (context) => geoLocatorService.getCurrentLocation(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'parking locator',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: search(),
+        home: MarkedPlaces(),
       ),
     );
   }
