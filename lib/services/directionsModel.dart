eec7b3c351c74424af8bdf249835d42ac4eebedd
shahyyehia <shahyyehia73@gmail.com>
2022-03-28 15:10:32 +0200

    direction implementation

--- lib/services/directionsModel.dart
+++ lib/services/directionsModel.dart
@@ -3,14 +3,14 @@ import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Directions {
-  final LatLngBounds bounds;
-  final List<PointLatLng> polylinePoints;
+  final String source;
+  final String target;
   final String totalDistance;
   final String totalDuration;
 
   const Directions({
-    @required this.bounds,
-    @required this.polylinePoints,
+    @required this.source,
+    @required this.target,
     @required this.totalDistance,
     @required this.totalDuration,
   });
@@ -22,27 +22,22 @@ class Directions {
     // Get route information
     final data = Map<String, dynamic>.from(map['routes'][0]);
 
-    // Bounds
-    final northeast = data['bounds']['northeast'];
-    final southwest = data['bounds']['southwest'];
-    final bounds = LatLngBounds(
-      northeast: LatLng(northeast['lat'], northeast['lng']),
-      southwest: LatLng(southwest['lat'], southwest['lng']),
-    );
-
     // Distance & Duration
     String distance = '';
     String duration = '';
+    String  end_address="";
+    String start_address="";
     if ((data['legs'] as List).isNotEmpty) {
       final leg = data['legs'][0];
       distance = leg['distance']['text'];
       duration = leg['duration']['text'];
+      end_address=leg["end_address"];
+      start_address=leg["start_address"];
     }
 
     return Directions(
-      bounds: bounds,
-      polylinePoints:
-      PolylinePoints().decodePolyline(data['overview_polyline']['points']),
+      source: start_address,
+      target:end_address,
       totalDistance: distance,
       totalDuration: duration,
     );
